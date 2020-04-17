55b3f86b7602f10f7fb42eb7023e7cabb36329a7
bridgeL <47290820+bridgeL@users.noreply.github.com>
2020-04-17 21:26:28 +0800

    command version gameroom
    enjoy!

--- client/test.py
+++ client/test.py
@@ -2,8 +2,18 @@ from collections import defaultdict
 
 site = defaultdict(list)
 
+site[4].append(1)
 site[0].append('sdf')
 site[1].append(1)
-site[4].append(1)
+
 
 print(site.__len__())
+
+site_order_list = []
+
+for k in site.keys():
+    site_order_list.append(k)
+
+site_order_list.sort()
+
+print(site_order_list)
--- server/app.py
+++ server/app.py
@@ -5,6 +5,24 @@ import communicate as cmc
 import gameroom
 
 
+def deal_cmd(word):
+    if word == '$exit':
+        return 1
+    elif word == '$close connect':
+        cmc.close()
+
+    gv.word_input = ''
+    return 0
+
+
+def deal_manager(msg):
+    game_mode = gv.game_mode
+    if game_mode == 0:
+        gameroom.answer_manager(msg)
+    elif game_mode == 2:
+        pass
+
+
 if __name__ == "__main__":
     print('test start...')
 
@@ -15,20 +33,17 @@ if __name__ == "__main__":
         word = gv.word_input
         if word:
             if word[0] == '$':
-                if word == '$exit':
+                if deal_cmd(word):
                     break
-                elif word == '$close connect':
-                    cmc.close()
-                    gv.word_input = ''
-            else:
-                m = gmsg.GAMEMSG(0, 0, 0, 0, word)
-                cmc.send(m)
-                gv.word_input = ''
+            # else:
+            #     m = gmsg.GAMEMSG(0, 0, 0, 0, word)
+            #     cmc.send(m)
+            #     gv.word_input = ''
 
         msg_list = gv.msg_list
         gv.msg_list = []
         for m in msg_list:
             print(m.MSG2BYTE())
-            gameroom.manager(m)
+            deal_manager(m)
 
     print('...test end')
